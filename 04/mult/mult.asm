2dd3d9dd5f3390ad50f686dd78cde009b8c8cf47
Francesco Aiello <aiellof@oregonstate.edu>
2019-02-13 15:44:04 -0800

    Multiplication added & passes test

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//Set Sum Register to 0
+@2
+M=0
+
+//Start loop
+(LOOP)
+//Check Break Condition
+@1
+D=M
+@END
+D;JEQ
+
+//Decrement counter
+@1
+M=M-1
+
+//Add to Sum
+@0
+D=M
+@2
+M=M+D
+@LOOP
+0;JMP
+
+//END program
+(END)
